b7c354746217cca307139e1869fa4652cee155f3
Nanda241998 <117671873+Nanda241998@users.noreply.github.com>
2023-03-14 23:11:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,5 +11,28 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/9",
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
+const timeTaken = {};
 
+		const fetchApiData = async (url) => {
+			const startTime = Date.now();
+			const response = await fetch(url);
+			const data = await response.json();
+			const endTime = Date.now();
+			return { data, time: endTime - startTime };
+		}
+
+		// Promise.all
+		const promiseAllStartTime = Date.now();
+		await Promise.all(apiUrls.map(fetchApiData));
+		const promiseAllEndTime = Date.now();
+		timeTaken.promiseAll = promiseAllEndTime - promiseAllStartTime;
+		document.getElementById('output-all').innerText = timeTaken.promiseAll;
+
+		// Promise.any
+		const promiseAnyStartTime = Date.now();
+		await Promise.any(apiUrls.map(fetchApiData));
+		const promiseAnyEndTime = Date.now();
+		timeTaken.promiseAny = promiseAnyEndTime - promiseAnyStartTime;
+		document.getElementById('output-any').innerText = timeTaken.promiseAny;
+	
 // You can write your code here
